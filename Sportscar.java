13dce831a721e88045876be9678f3c6f26fa2811
Yadukrishnan Nair <dreamlight7170@gmail.com>
2026-08-03 12:59:23 +0530

    java - program (03/08/2026)

---
+++ Sportscar.java
@@ -0,0 +1,13 @@
+public class Sportscar
+{
+    public static void main(String [] args)
+    {
+        Vehicle Sportscar = new Vehicle();
+        Sportscar.setname("name - \"Porsche 911\"");
+        Sportscar.setbrand("brand - \"Porsche\"");
+        Sportscar.setmodel(911);
+        Sportscar.setcolor("color - \"Grey\"");
+        Sportscar.setprice(250000);
+    }
+    
+}
---
+++ Vehicle.java
@@ -0,0 +1,31 @@
+public class Vehicle
+{
+    private String name;
+    private String brand;
+    private int model;
+    private String color;
+    private int price;
+
+    public void setname(String name)
+    {
+        System.out.println(name);
+    }
+    public void setbrand(String brand)
+    {
+        System.out.println(brand);
+    }
+    public void setcolor(String color)
+    {
+        System.out.println(color);
+    }
+    public void setmodel(int model)
+    {
+        System.out.println(model);
+    }
+    public void setprice(int price)
+    {
+        System.out.println(price);
+    }
+
+    
+}
